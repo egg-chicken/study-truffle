d721077cbadcefa70ba8f38230e80f91297c6529
Ken Eguchi <eguchi@haw.co.jp>
2018-03-07 14:58:54 +0900

    Use beforeEach

--- test/orecoin.js
+++ test/orecoin.js
@@ -1,15 +1,19 @@
 const OreCoin = artifacts.require("./OreCoin.sol");
 
 contract('OreCoin', (accounts) => {
+  let orecoin;
+
+  beforeEach(async () => {
+    orecoin = await OreCoin.deployed();
+  });
+
   it('should put 10000 oc in the first account', async () => {
-    const orecoin = await OreCoin.deployed();
     const balance = await orecoin.getBalance.call(accounts[0]);
     expect(balance.toNumber()).to.equal(10000);
   });
 
   describe('sendCoin', () => {
     it('should send 10 oc', async () => {
-      const orecoin = await OreCoin.deployed();
       await orecoin.sendCoin(accounts[1], 10, { from: accounts[0] });
       const sender = await orecoin.getBalance.call(accounts[0]);
       const receiver = await orecoin.getBalance.call(accounts[1]);
@@ -18,7 +22,6 @@ contract('OreCoin', (accounts) => {
     });
 
     it('should send event', async () => {
-      const orecoin = await OreCoin.deployed();
       const result = await orecoin.sendCoin(accounts[1], 10, { from: accounts[0] });
       const events = result.logs.map((log) => log.event);
       expect(events).to.include('SendCoin');
