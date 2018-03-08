1c1f70d1ad073de5b790d6052a788e479c16f2e3
Ken Eguchi <eguchi@haw.co.jp>
2018-03-08 18:25:12 +0900

    Create Members contract for customize the discount rule

---
+++ contracts/Members.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.4.17;
+
+import "./Owned.sol";
+
+contract Members is Owned {
+    Rank[] public ranks;
+    mapping (address => Member) public members;
+
+    struct Member {
+        uint transactionCount;
+        uint transactionAmount;
+        uint rankIndex;
+    }
+
+    struct Rank {
+        string name;
+        uint minCount;
+        uint minAmount;
+        uint8 discountRate;
+    }
+
+    function Members() public {
+        pushRank('Default', 0, 0, 0);
+    }
+
+    function pushRank(string name, uint minCount, uint minAmount, uint8 discountRate) onlyOwner public {
+        ranks.push(Rank(name, minCount, minAmount, discountRate));
+    }
+
+    function editRank(uint index, string name, uint minCount, uint minAmount, uint8 discountRate) onlyOwner public {
+        ranks[index] = Rank(name, minCount, minAmount, discountRate);
+    }
+
+    function addTransactionAmount(address memberAddr, uint amount) public {
+        Member storage member = members[memberAddr];
+        member.transactionCount += 1;
+        member.transactionAmount += amount;
+        for(uint i=0; i<ranks.length; i++) {
+            if(ranks[i].minCount <= member.transactionCount &&
+               ranks[i].minAmount <= member.transactionAmount &&
+               ranks[i].discountRate > ranks[member.rankIndex].discountRate) {
+                member.rankIndex = i;
+            }
+        }
+    }
+
+    function getDiscountRate(address memberAddr) view public returns(uint8 discountRate){
+        uint rankIndex = members[memberAddr].rankIndex;
+        return ranks[rankIndex].discountRate;
+    }
+}
---
+++ test/members.js
@@ -0,0 +1,46 @@
+const Members = artifacts.require("./Members.sol");
+
+contract('Members', (accounts) => {
+  let members;
+
+  beforeEach(async () => {
+    members = await Members.new();
+  });
+
+  describe('pushRank', () => {
+    it('should push bronze rank', async () => {
+      await members.pushRank('Bronze', 1, 5, 10);
+      const rank = await members.ranks.call(1);
+      expect(rank[0]).to.equal('Bronze');
+      expect(rank[1].toNumber()).to.equal(1);
+      expect(rank[2].toNumber()).to.equal(5);
+      expect(rank[3].toNumber()).to.equal(10);
+    });
+  });
+
+  describe('addTransactionAmount', () => {
+    it('should update transactionCount, transactionAmoount and rankIndex', async () => {
+      await members.pushRank('Bronze', 1, 5, 10);
+      await members.addTransactionAmount(accounts[0], 2);
+      await members.addTransactionAmount(accounts[0], 3);
+      const member = await members.members.call(accounts[0]);
+      expect(member[0].toNumber()).to.equal(2);
+      expect(member[1].toNumber()).to.equal(5);
+      expect(member[2].toNumber()).to.equal(1);
+    });
+  });
+
+  describe('getDiscountRate', () => {
+    it('should return 0 when they are default user', async () => {
+      const rate = await members.getDiscountRate.call(accounts[0]);
+      expect(rate.toNumber()).to.equal(0);
+    });
+
+    it('should return 10 when they are special user', async () => {
+      await members.pushRank('Bronze', 1, 5, 10);
+      await members.addTransactionAmount(accounts[0], 1000);
+      const rate = await members.getDiscountRate.call(accounts[0]);
+      expect(rate.toNumber()).to.equal(10);
+    });
+  });
+});
